eda6bfe54fbc263763ce0f27154a58ff2fe7d93a
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-06 09:19:34 -0300

    script create table

---
+++ scripts/CreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE phpcrud.`data` (
+	id INT auto_increment NULL,
+	name varchar(100) NULL,
+	location varchar(100) NULL
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='
+
+'
+AUTO_INCREMENT=1;
