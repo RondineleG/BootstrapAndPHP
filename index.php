d8ab3c882cb887e57a0dc820db08808d6cbc38e0
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-06 09:00:24 -0300

    add bootstrap to form

--- index.php
+++ index.php
@@ -6,24 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Algorithms PHP</title>
-    
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-   
+
     <link href="/src/css/main.css" rel="stylesheet">
 </head>
 
 <body>
-    <form action="" method="POST">
-     <label>Name :</label>
-    <input type="text" name="name" value="Enter your name">
-    <label>Location :</label>
-    <input type="text" name="location" value="Enter your location">
-    <button type="submit" name="save">Save</button>
-    </form>
-     
-         <script src="/src/js/main.js"></script>     
+
+    <div class="row justify-content-center">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label>Name :</label>
+                <input type="text" name="name" class="form-control" value="Enter your name">
+            </div>
+
+            <div class="form-group">
+                <label>Location :</label>
+                <input type="text" name="location" class="form-control" value="Enter your location">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="save">Save</button>
+            </div>
+        </form>
+    </div>
+
+    <script src="/src/js/main.js"></script>
 </body>
 
 </html>
